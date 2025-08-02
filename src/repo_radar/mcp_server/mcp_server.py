07dca5201a8d69674ba4320283d3b0a476789834
Karthick Shanmuga Rao <karthikssfeb@gmail.com>
2025-08-02 23:28:41 +0530

    Testing MCP server

--- src/repo_radar/mcp_server/mcp_server.py
+++ src/repo_radar/mcp_server/mcp_server.py
@@ -9,6 +9,11 @@ from repo_radar.mcp_server.tool_loader import mcp, load_tools_for_mcp
 
 load_tools_for_mcp()
 
+
+def main():
+    mcp.run(transport="http", host="127.0.0.1", port=8000)
+
+
 if __name__ == "__main__":
     # uvicorn.run(mcp, host="0.0.0.0", port=8000)
-    mcp.run(transport="http", host="127.0.0.1", port=8000)
+    main()
